e1ec83e25fbf36ccec466573de6d6ef5adfeca89
Lucas Silva <lpcs3535@gmail.com>
2025-01-08 19:08:53 +0000

    add mocks

--- server.go
+++ server.go
@@ -1,34 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"net/url"
 )
 
 type OutboundRequest struct {
-	Method string
-	URL    *url.URL
-	Header map[string][]string
-	Body   []byte
+	Method string              `json:"method"`
+	URL    string              `json:"url"`
+	Header map[string][]string `json:"header"`
+	Body   string              `json:"body"`
 }
 
 type OutboundRequestResponse struct {
-	StatusCode int
-	Header     map[string][]string
-	Body       []byte
+	StatusCode int                 `json:"statusCode"`
+	Header     map[string][]string `json:"header"`
+	Body       string              `json:"body"`
 }
 
 type RecordedRequest struct {
-	outboundRequest         OutboundRequest
-	outboundRequestResponse OutboundRequestResponse
+	OutboundRequest         OutboundRequest         `json:"outboundRequest"`
+	OutboundRequestResponse OutboundRequestResponse `json:"outboundRequestResponse"`
 }
 
 type State struct {
 	recordedRequests []RecordedRequest
-	mocks            []http.Request
+	mocks            []RecordedRequest
 }
 
 func (s *State) saveRequest(r RecordedRequest) {
@@ -37,69 +37,59 @@ func (s *State) saveRequest(r RecordedRequest) {
 
 func buildProxyHandler(state *State) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		handleProxy(w, r, state)
-	}
-}
+		r.Header.Del("Proxy-Connection")
 
-func handleProxy(w http.ResponseWriter, r *http.Request, state *State) {
-	r.Header.Del("Proxy-Connection")
+		fmt.Printf("proxy: %s %s\n", r.Method, r.URL.String())
 
-	fmt.Printf("proxy: %s %s\n", r.Method, r.URL.String())
+		requestBody, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Fatalf("Failed to read request body: %v", err)
+		}
+		fmt.Printf("request body %s\n", requestBody)
 
-	requestBody, err := io.ReadAll(r.Body)
-	if err != nil {
-		log.Fatalf("Failed to read request body: %v", err)
-	}
-	fmt.Printf("request body %s\n", requestBody)
+		resp, err := http.DefaultTransport.RoundTrip(r)
+		if err != nil {
+			state.saveRequest(RecordedRequest{
+				OutboundRequest: OutboundRequest{
+					Method: r.Method,
+					URL:    r.URL.String(),
+					Header: r.Header,
+					Body:   string(requestBody),
+				},
+				OutboundRequestResponse: OutboundRequestResponse{
+					StatusCode: -1,
+					Header:     nil,
+					Body:       "",
+				},
+			})
+			http.Error(w, err.Error(), http.StatusBadGateway)
+			return
+		}
 
-	resp, err := http.DefaultTransport.RoundTrip(r)
-	if err != nil {
-		// responseBody, err := io.ReadAll(resp.Body)
-		// if err != nil {
-		// 	log.Fatalf("Failed to read response body: %v", err)
-		// }
-		// fmt.Printf("response body %s\n", responseBody)
+		defer resp.Body.Close()
 
+		copyHeader(w.Header(), resp.Header)
+		w.WriteHeader(resp.StatusCode)
+		io.Copy(w, resp.Body)
+
+		responseBody, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalf("Failed to read response body: %v", err)
+		}
 		state.saveRequest(RecordedRequest{
-			outboundRequest: OutboundRequest{
+			OutboundRequest: OutboundRequest{
 				Method: r.Method,
-				URL:    r.URL,
+				URL:    r.URL.String(),
 				Header: r.Header,
-				Body:   requestBody,
+				Body:   string(requestBody),
 			},
-			outboundRequestResponse: OutboundRequestResponse{
-				StatusCode: -1,
-				Header:     nil,
-				Body:       nil,
+			OutboundRequestResponse: OutboundRequestResponse{
+				StatusCode: resp.StatusCode,
+				Header:     resp.Header,
+				Body:       string(responseBody),
 			},
 		})
-		http.Error(w, err.Error(), http.StatusBadGateway)
-		return
 	}
-
-	defer resp.Body.Close()
-
-	copyHeader(w.Header(), resp.Header)
-	w.WriteHeader(resp.StatusCode)
-	io.Copy(w, resp.Body)
-
-	responseBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("Failed to read response body: %v", err)
-	}
-	state.saveRequest(RecordedRequest{
-		outboundRequest: OutboundRequest{
-			Method: r.Method,
-			URL:    r.URL,
-			Header: r.Header,
-			Body:   requestBody,
-		},
-		outboundRequestResponse: OutboundRequestResponse{
-			StatusCode: resp.StatusCode,
-			Header:     resp.Header,
-			Body:       responseBody,
-		},
-	})
 }
 
 func copyHeader(dst, src http.Header) {
@@ -110,24 +100,56 @@ func copyHeader(dst, src http.Header) {
 	}
 }
 
+func buildAddMockHandler(state *State) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		requestBody, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Fatalf("Failed to read request body: %v", err)
+		}
+		fmt.Printf("\n\nadding mock request body\n %s\n\n", requestBody)
+
+		var recordedRequest RecordedRequest
+		err = json.Unmarshal(requestBody, &recordedRequest)
+		if err != nil {
+			panic(err)
+		}
+
+		state.mocks = append(state.mocks, recordedRequest)
+
+        toReturn, err := json.Marshal(recordedRequest)
+        if err != nil {
+            panic(err)
+        }
+
+		fmt.Printf("\n\nadded mock request body\n %s\n\n", string(toReturn))
+		fmt.Fprintf(w, "%v", string(toReturn))
+	}
+}
+
+func buildMocksHandler(state *State) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+        toReturn, err := json.Marshal(state.mocks)
+        if err != nil {
+            panic(err)
+        }
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, "%v", string(toReturn))
+	}
+}
+
 func buildRecordedRequestsHandler(state *State) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("recordedRequests")
 		fmt.Println(state)
-		fmt.Fprintf(w, "Recorded requests: %v", state.recordedRequests)
-	}
-}
-
-func buildAddMockRequestHandler(state *State) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Recorded requests: %v", state.recordedRequests)
+		fmt.Fprintf(w, "Current recorded requests in state: %v", state.recordedRequests)
 	}
 }
 
 func main() {
 	state := &State{}
 
-	// http.HandleFunc("/addMock", buildRecordedRequestsHandler(state))
+	http.HandleFunc("/addMock", buildAddMockHandler(state))
+	http.HandleFunc("/mocks", buildMocksHandler(state))
 	http.HandleFunc("/recordedRequests", buildRecordedRequestsHandler(state))
 	http.HandleFunc("/", buildProxyHandler(state))
 
